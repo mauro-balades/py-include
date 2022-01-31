1a68f1e6a85ec4780812af37a960278fb120f53c
Mauro Baladés <mauro.balades@tutanota.com>
2022-01-31 16:45:17 +0000

    Added initiation files

---
+++ setup.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python3
+
+"""
+| --------------------- Py include <Mauro Baladés> ---------------------
+|  ___   _         _   _      __    _     _     ___   ____ 
+| | |_) \ \_/     | | | |\ | / /`  | |   | | | | | \ | |_  
+| |_|    |_|      |_| |_| \| \_\_, |_|__ \_\_/ |_|_/ |_|__ 
+| ----------------------------------------------------------------------
+| MIT License
+| 
+| Copyright (c) 2022 Mauro Baladés
+|
+| Permission is hereby granted, free of charge, to any person obtaining a copy
+| of this software and associated documentation files (the "Software"), to deal
+| in the Software without restriction, including without limitation the rights
+| to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+| copies of the Software, and to permit persons to whom the Software is
+| furnished to do so, subject to the following conditions:
+|
+| The above copyright notice and this permission notice shall be included in all
+| copies or substantial portions of the Software.
+|
+| THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+| IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+| FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+| AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+| LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+| OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+| SOFTWARE.
+|
+"""
+
+from setuptools import setup, find_packages
+
+__version__ = "1.0.0"
+
+setup_info = dict(
+    name='py-include',
+    version=__version__,
+    author='Mauro Baladés',
+    author_email='mauro.balades@tutanota.com',
+    url='https://github.com/mauro-balades/py-include',
+    project_urls={
+        'Source': 'https://github.com/mauro-balades/py-include',
+    },
+    description='Inport your python files in runtime by paths',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: MacOS X',
+        'Environment :: Win32 (MS Windows)',
+        'Environment :: X11 Applications',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Topic :: Games/Entertainment',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+
+    # Package info
+    packages=['py-include'],
+
+    # Add _ prefix to the names of temporary build dirs
+    options={'build': {'build_base': '_build'}, },
+    zip_safe=True,
+)
+
+setup(**setup_info)
